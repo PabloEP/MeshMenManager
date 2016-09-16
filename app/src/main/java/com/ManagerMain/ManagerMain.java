1490cba342000720c97a456583b15c15ed3953df
Pablo <pabloep96@gmail.com>
2016-09-16 06:17:39 -0600

    Initial commit

---
+++ app/src/main/java/com/ManagerMain/ManagerMain.java
@@ -0,0 +1,13 @@
+package com.ManagerMain;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManagerMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager_main);
+    }
+}
